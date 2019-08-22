d5ea11ed43ab9e8ea0631b5c2957d49a4006347f
Luv Aidasani <laidasani@gmail.com>
2019-08-22 14:26:25 -0700

    Successfully Loaded Static Resources

---
+++ onlineshopping-FrontEnd/src/main/java/net/lka/onlineshoppingFrontEnd/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.lka.onlineshoppingFrontEnd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","home","index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring Web MVC");
+		return mv;
+	}
+
+}
