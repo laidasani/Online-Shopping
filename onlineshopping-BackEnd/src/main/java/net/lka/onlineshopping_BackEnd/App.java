8fd0a9fd32a0f78a89bd384e2dc684c1911cf473
Luv Aidasani <laidasani@gmail.com>
2019-08-21 17:37:12 -0700

    Project Setup Completed Successfully.

---
+++ onlineshopping-BackEnd/src/main/java/net/lka/onlineshopping_BackEnd/App.java
@@ -0,0 +1,13 @@
+package net.lka.onlineshopping_BackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
